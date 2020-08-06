c42f5eb83c5a5f4acbef1badcd5198962f51342f
Tran Viet <reddevil241097@gmail.com>
2020-08-06 16:23:42 +0700

    date

--- routes/index.js
+++ routes/index.js
@@ -60,27 +60,63 @@ router.get('/checkphone/:sdt', (req, res) => {
 //end checkphone
 
 //horoscope
-router.get('/horoscope/:namsinh', (req, res) => {
-    var birthYear = extractBirthYear(req.params.namsinh);
+router.get('/horoscope/:namsinh', async (req, res) => {
+    var birthYear = await extractBirthYear(req.params.namsinh);
     if (birthYear == 0) {
         res.send({
             "redirect_to_blocks": ["tuvi khongconamsinh"]
         });
     } else {
+        var tuvi = require('./tuvi.json');
+        let lists = [];
+        let laso = (birthYear - 1870) % 12;
+        tuvi[laso].forEach(element => {
+            lists.push({"text": element});
+        });
         res.send({
-            "redirect_to_blocks": ["tuvi doctuvi1"]
+            "message": lists
         });
     }
 });
 //end horoscope
 
+router.get('/get-info/get-birth', (req, res) => {
+    let info = req.url.split('info=')[1];
+    let arr = info.split('/');
+    console.log(arr);
+    info = arr[1] + '/' + arr[0] + '/' + arr[2];
+    let date = new Date(info);
+    if(date == 'Invalid Date') {
+        res.send({
+            "redirect_to_blocks": ["nhapsaingaysinh"]
+        });
+    } else {
+        res.send({
+            "redirect_to_blocks": ["nhapdungngaysinh"]
+        });
+    }
+})
+
 //function
 //lay nam sinh ra tu input cua nguoi dung
 var extractBirthYear = _userInput => {
-    if (!hasNumber(_userInput)) {
-        return 0
-    }
-    return 1;
+    return new Promise(async (resolve, reject) => {
+        if (!hasNumber(_userInput)) {
+            resolve(0);
+        } else {
+            if(_userInput >= 2) {
+                _userInput = Number(_userInput.slice(-2));
+                if(_userInput >= 0 && _userInput <= 20) {
+                    _userInput += 2000;
+                } else {
+                    _userInput += 1900;
+                }
+                resolve(_userInput);
+            } else {
+                resolve(0);
+            }
+        }
+    })
 }
 var hasNumber = str => {
     return /\d/.test(str);
@@ -103,9 +139,11 @@ var checkPhone = (_phone) => {
             if (_phone.length < 11 || _phone.length > 12) return false;
             return true;
         }
-    } else {
+    } else if(_phone[0] == "0") {
         if (_phone.length < 10 || _phone.length > 11) return false;
         return true;
+    } else{
+        return false;
     }
 }
 
